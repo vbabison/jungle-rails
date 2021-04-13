e32343e6da9bd5d3c325ed7c8a19f1dc31114752
Babison Vilvarajah <officialbookingssf@gmail.com>
2021-04-13 18:32:09 -0400

    User authentication error message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,22 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
-    puts user_params
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to '/'
     else
-      redirect_to '/signup'
+      render :new
     end
   end
 
 private
 
   def user_params
-    params.require(:user).permit(:firstname, :lastname, :email, :password, :password_confirmation)
+    params.permit(:firstname, :lastname, :email, :password, :password_confirmation)
   end
 end
